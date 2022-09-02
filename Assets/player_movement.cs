b6007dfc68920527de6d00d50ef6695e1f710860
ameliaruthmoore <ameliamoore@live.ca>
2022-09-01 23:26:36 -0400

    Sep 1 work -- new assets, general story, player can now move, placement of character dialogue busts

---
+++ Assets/player_movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        gameObject.transform.position = new Vector2(transform.position.x + x*speed, 
+        transform.position.y + y*speed);
+    }
+}
